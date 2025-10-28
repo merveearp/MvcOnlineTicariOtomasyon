48560d4e02d833500d57b9581ad1b4e1e3be6d29
Merve Arpacıoğlu Türk <merveearp@icloud.com>
2025-10-29 00:03:11 +0300

    Add project files.

---
+++ MvcOnlineTicariOtomasyon/Models/Sınıflar/Personel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTicariOtomasyon.Models.Sınıflar
+{
+    public class Personel
+    {
+        [Key]
+        public int PersonelId { get; set; }
+
+        [Column(TypeName = "Varchar")]
+        [StringLength(20)]
+        public string PersonelAd { get; set; }
+
+        [Column(TypeName = "Varchar")]
+        [StringLength(20)]
+        public string PersonelSoyad { get; set; }
+
+        [Column(TypeName = "Varchar")]
+        [StringLength(500)]
+        public string PersonelGörsel { get; set; }
+
+        public SatisHareket SatisHareket { get; set; }
+        public Departman Departman { get; set; }
+    }
+}
---
+++ MvcOnlineTicariOtomasyon/Models/Sınıflar/Urun.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTicariOtomasyon.Models.Sınıflar
+{
+    public class Urun
+    {
+        [Key]
+        public int UrunId { get; set; }
+
+        [Column(TypeName ="Varchar")]
+        [StringLength(30)]
+        public string UrunAd { get; set; }
+
+        [Column(TypeName = "Varchar")]
+        [StringLength(30)]
+        public string Marka { get; set; }
+        public short Stok { get; set; }
+        public decimal AlisFiyat { get; set; }
+        public decimal SatisFiyat { get; set; }
+        public bool Durum { get; set; }
+
+        [Column(TypeName = "Varchar")]
+        [StringLength(300]
+        public string UrunGörsel { get; set; }
+
+
+        public Kategori Kategori { get; set; }
+        public SatisHareket SatisHareket { get; set; }
+
+    }
+}
